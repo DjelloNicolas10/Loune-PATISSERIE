d48db02b4e1f577e9f7d6ff8d724681fb40f34f0
Djello Nicolas <djellodesignerweb@gmail.com>
2025-01-27 14:44:46 -0200

    Update

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// Seleção dos elementos
+const mobileBtn = document.getElementById('mobile_btn');
+const closeButton = document.getElementById('closeButton');
+const mobileMenu = document.getElementById('mobile_menu');
+
+// Abrir o menu móvel
+mobileBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('active');
+});
+
+// Fechar o menu móvel
+closeButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+});
+
+
+
+
+
+// Movimentação dos items
+const sections = $('section');
+const navItems = $('.nav-item');
+$(window).on('scroll', function () {
+    const header = $('header');
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+    let activeSectionIndex = 0;
+    if (scrollPosition <= 0) {
+        header.css('box-shadow', 'none');
+    } else {
+        header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+    }
+    sections.each(function(i) {
+        const section = $(this);
+        const sectionTop = section.offset().top - 96;
+        const sectionBottom = sectionTop+ section.outerHeight();
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            activeSectionIndex = i;
+            return false;
+        }
+    })
+    navItems.removeClass('active');
+    $(navItems[activeSectionIndex]).addClass('active');
+});
+ScrollReveal().reveal('#cta', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.dish', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('#testimonial_chef', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+})
+ScrollReveal().reveal('.feedback', {
+    origin: 'right',
+    duration: 1000,
+    distance: '20%'
+});
